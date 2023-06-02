270fd7d4782c8cb2481988d2407866fbee4efa7a
Arwin Yella <16850195+arwin4@users.noreply.github.com>
2023-06-02 14:05:49 +0200

    Add append method

--- linkedList.js
+++ linkedList.js
@@ -12,12 +12,36 @@ class LinkedList {
     return this.listSize;
   }
 
+  addOne() {
+    this.listSize += 1;
+  }
+
   prepend(value) {
     const newNode = new Node();
     newNode.value = value;
     newNode.nextNode = this.headNode;
     this.headNode = newNode;
-    this.listSize += 1;
+    this.addOne();
+  }
+
+  append(value) {
+    const newNode = new Node();
+    newNode.value = value;
+    newNode.nextNode = null;
+
+    if (this.headNode === null) {
+      // List is empty
+      this.headNode = newNode;
+    } else {
+      // Traverse the list to find the last node
+      let currentNode = this.headNode;
+      while (currentNode.nextNode !== null) {
+        // Keep going until the node that points to null is found
+        currentNode = currentNode.nextNode;
+      }
+      currentNode.nextNode = newNode;
+    }
+    this.addOne();
   }
 
   head() {
--- main.js
+++ main.js
@@ -4,11 +4,10 @@ import LinkedList from './linkedList.js';
 const list1 = new LinkedList('A first list');
 
 // console.log(list1);
-list1.prepend('firstone');
-list1.prepend('another');
-list1.prepend('another');
-list1.prepend('another');
-list1.prepend('another');
-list1.prepend('another');
 
 console.log(list1.size());
+list1.append('first one');
+list1.append('second one');
+list1.prepend('one in front');
+console.log(list1.size());
+console.log(list1.head());
