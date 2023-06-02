1bf76ab7db1105084be964e1233a98f8cac373b0
Arwin Yella <16850195+arwin4@users.noreply.github.com>
2023-06-02 14:37:22 +0200

    Add tail method
     - Refactor append() to use tail().
     - Make addOne() private and give it a more verbose name.
     - Add comments.

--- linkedList.js
+++ linkedList.js
@@ -12,39 +12,56 @@ class LinkedList {
     return this.listSize;
   }
 
-  addOne() {
+  #increaseSizeByOne() {
     this.listSize += 1;
   }
 
+  tail() {
+    // Traverse the list and return the last node
+
+    // List is empty
+    if (this.headNode === null) return this.headNode;
+
+    // List isn't empty
+    let currentNode = this.headNode;
+    while (currentNode.nextNode !== null) {
+      // Keep going until the node that points to null is found
+      currentNode = currentNode.nextNode;
+    }
+    return currentNode;
+  }
+
   prepend(value) {
+    // Add a node to the start of the list
+
     const newNode = new Node();
     newNode.value = value;
     newNode.nextNode = this.headNode;
     this.headNode = newNode;
-    this.addOne();
+    this.#increaseSizeByOne();
   }
 
   append(value) {
+    // Add a node to the end of the list
+
     const newNode = new Node();
     newNode.value = value;
     newNode.nextNode = null;
 
-    if (this.headNode === null) {
-      // List is empty
+    const tail = this.tail();
+    if (tail === null) {
+      // If there's no tail (the list is empty) make this node the first
       this.headNode = newNode;
     } else {
-      // Traverse the list to find the last node
-      let currentNode = this.headNode;
-      while (currentNode.nextNode !== null) {
-        // Keep going until the node that points to null is found
-        currentNode = currentNode.nextNode;
-      }
-      currentNode.nextNode = newNode;
+      // Point the current tail to this new tail
+      tail.nextNode = newNode;
     }
-    this.addOne();
+
+    this.#increaseSizeByOne();
   }
 
   head() {
+    // Return the first node
     return this.headNode;
   }
 }
--- main.js
+++ main.js
@@ -3,11 +3,12 @@ import LinkedList from './linkedList.js';
 
 const list1 = new LinkedList('A first list');
 
-// console.log(list1);
+console.log(list1.size());
 
-console.log(list1.size());
-list1.append('first one');
-list1.append('second one');
-list1.prepend('one in front');
-console.log(list1.size());
+list1.append('one');
+list1.append('one after');
+list1.prepend('0');
+list1.append('4');
+console.log(list1.tail());
 console.log(list1.head());
+console.log(list1.size());
